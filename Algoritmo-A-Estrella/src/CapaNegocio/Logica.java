2aac09b63da25a917b5655e380844330f0393dff
WilsonIsAlive <33021153+WilsonIsAlive@users.noreply.github.com>
2019-02-19 18:59:01 +0100

    Mejorando

--- Algoritmo-A-Estrella/src/CapaNegocio/Logica.java
+++ Algoritmo-A-Estrella/src/CapaNegocio/Logica.java
@@ -1,6 +1,7 @@
 package CapaNegocio;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Logica {
 	private int dimensionN;
@@ -9,6 +10,8 @@ public class Logica {
 	private ArrayList<TNodo> abierta;
 	private ArrayList<TNodo> cerrada;
 	private Coordenadas meta;
+	private Coordenadas inicio;
+	private Coordenadas actual;
 	
 	public Logica(int n, int m){
 		// Inicializo los atributos 
@@ -20,14 +23,44 @@ public class Logica {
 		
 		for(int i = 0; i < n; i++){
 			for(int j = 0;j < m; j++){
-				this.matriz[i][j] = new TNodo(false, -1, -1, -1);
+				this.matriz[i][j] = new TNodo(false, -1, -1, -1, "Normal");
 			}
 		}
 		this.generarTableroAleatoriamente();
+		this.calcularCamino();
 	}
 	
 	public void generarTableroAleatoriamente(){
-		
+		int tam = this.dimensionM*this.dimensionN;
+		this.inicio.setI((int) (Math.random()*tam));
+		this.inicio.setJ((int) (Math.random()*tam));
+		this.meta.setI((int) (Math.random()*tam));
+		this.meta.setJ((int) (Math.random()*tam));
+		// Para que no salga un mismo punto de Inicio y de Meta
+		while((this.meta.getI()!=this.inicio.getI()) && (this.meta.getJ()!= this.inicio.getJ())){
+			this.meta.setI((int) (Math.random()*tam));
+			this.meta.setJ((int) (Math.random()*tam));
+		}
+		// Ya generados los puntos de Inicio y Meta		
+	}
+	public void calcularCamino(){
+		int posI, posJ;
+		boolean salir = false;
+		this.actual = this.inicio;
+	
+		while(this.actual != this.meta && !salir){
+			
+			for(int i= 0; i < 8; i++){
+				if(i == 0){// Arriba a la izquierda
+					posI = this.actual.getI()-1;
+					posJ = this.actual.getJ()-1;
+					if(posI >= 0){// Esta bien
+						
+					}
+				}
+				
+			}
+		}
 	}
 	
 	
--- Algoritmo-A-Estrella/src/CapaNegocio/TNodo.java
+++ Algoritmo-A-Estrella/src/CapaNegocio/TNodo.java
@@ -2,22 +2,26 @@ package CapaNegocio;
 
 public class TNodo {
 	private boolean usado;
-	private double h;// Distancia al nodo actual
-	private double g;// Distancia al nodo meta
-	private double distancia;// es la f() = h()+g()
+	private double h; 	// Formula h(n) = sqrt((x1+x2)^2+(y1+y2)^2)
+	private double g;	// Formula g(n) = SUMA(h(n))
+	private double f;	// Formula f(n) = h(n) + g(n)
+	private String tipo;
 	
-	public TNodo(boolean usado, double h, double g, double distancia){
+	public TNodo(boolean usado, double h, double g, double f, String tipo){
 		this.usado = usado;
 		this.h = h;
 		this.g = g;
-		this.distancia = distancia;
-		this.setUsado(usado);
-		this.setH(h);
-		this.setG(g);
-		this.setDistancia(distancia);
+		this.f = f;
+		this.tipo = tipo;
 	}
 
 	// Empiezan los getters y los setters
+	public String getTipo(){
+		return this.tipo;
+	}
+	public void setTipo(String tipo){
+		this.tipo = tipo;
+	}
 	public boolean getUsado() {
 		return this.usado;
 	}
@@ -43,11 +47,15 @@ public class TNodo {
 	}
 
 	public double getDistancia() {
-		return this.distancia;
+		return this.f;
 	}
 
-	public void setDistancia(double distancia) {
-		this.distancia = distancia;
+	public void setDistancia(double f) {
+		this.f = f;
 	}
 	// Acaban los setter y getters
+	
+	
+	
+	
 }
