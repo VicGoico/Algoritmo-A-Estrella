c5344ce1c6c96a17f8fb07d9d1d5de3a652f7210
WilsonIsAlive <33021153+WilsonIsAlive@users.noreply.github.com>
2019-02-20 17:50:18 +0100

    Falta ponerle los mensajes
    Para que pinte los datos por pantalla y poder saber si lo esta haciendo bien.

--- Algoritmo-A-Estrella/src/CapaNegocio/Logica.java
+++ Algoritmo-A-Estrella/src/CapaNegocio/Logica.java
@@ -1,32 +1,53 @@
 package CapaNegocio;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Logica {
 	private int dimensionN;
 	private int dimensionM;
 	private TNodo[][] matriz;
-	private ArrayList<TNodo> abierta;
+	//private ArrayList<TNodo> abierta;
 	private ArrayList<TNodo> cerrada;
 	private Coordenadas meta;
 	private Coordenadas inicio;
 	private Coordenadas actual;
 	
+	// Tiene que ordenar en funcion de la f() de menor a mayor
+	private PriorityQueue <TNodo> abierta;
+	
 	public Logica(int n, int m){
 		// Inicializo los atributos 
 		this.dimensionM = m;
 		this.dimensionN = n;
-		this.abierta = new ArrayList<>();
+		Comparator<TNodo> comparardor = new Comparator<TNodo>() {
+
+			@Override
+			public int compare(TNodo o1, TNodo o2) {
+				if(o1.getF() < o2.getF()){
+					return 0;// A lo mejor -1
+				}
+				return 1;
+			}
+			
+		};
+		this.abierta = new PriorityQueue<>(comparardor);
 		this.cerrada = new ArrayList<>();
 		this.matriz = new TNodo[n][m];
 		
+		this.generarTableroAleatoriamente();
+		// Falta poner zonas de paso: prohibidas, con penalizacion, etc
 		for(int i = 0; i < n; i++){
 			for(int j = 0;j < m; j++){
-				this.matriz[i][j] = new TNodo(false, -1, -1, -1, "Normal");
+				Coordenadas c = new Coordenadas(i,j);
+				TNodo nodo = new TNodo(false, 0, 0, 0, "Normal", c);
+				
+				this.matriz[i][j] = nodo;
+				
 			}
 		}
-		this.generarTableroAleatoriamente();
+		
 		this.calcularCamino();
 	}
 	
@@ -34,6 +55,11 @@ public class Logica {
 		int tam = this.dimensionM*this.dimensionN;
 		this.inicio.setI((int) (Math.random()*tam));
 		this.inicio.setJ((int) (Math.random()*tam));
+		TNodo aux = this.matriz[this.inicio.getI()][this.inicio.getJ()];
+		aux.calcularH(this.inicio, this.meta);
+		aux.calcularG();
+		aux.calcularF();
+		
 		this.meta.setI((int) (Math.random()*tam));
 		this.meta.setJ((int) (Math.random()*tam));
 		// Para que no salga un mismo punto de Inicio y de Meta
@@ -44,24 +70,94 @@ public class Logica {
 		// Ya generados los puntos de Inicio y Meta		
 	}
 	public void calcularCamino(){
-		int posI, posJ;
 		boolean salir = false;
+		Coordenadas aux = new Coordenadas(0,0);
+		TNodo coge;
 		this.actual = this.inicio;
-	
+		
+		this.abierta.add(this.matriz[this.actual.getI()][this.actual.getJ()]);	
+		this.cerrada.add(this.abierta.peek());
+		this.abierta.poll();
+		
 		while(this.actual != this.meta && !salir){
-			
-			for(int i= 0; i < 8; i++){
-				if(i == 0){// Arriba a la izquierda
-					posI = this.actual.getI()-1;
-					posJ = this.actual.getJ()-1;
-					if(posI >= 0){// Esta bien
-						
+			// Miro que este usado o no, si esta usado es que ya esta en la lista cerrada
+			if(!this.matriz[this.actual.getI()][this.actual.getJ()].getUsado()){
+				this.matriz[this.actual.getI()][this.actual.getJ()].setUsado(true);
+				for(int i= 0; i < 8; i++){
+					if(i == 0){// Arriba a la izquierda
+						aux.setI(this.actual.getI()-1);
+						aux.setJ(this.actual.getJ()-1);
+						if(aux.getI() >= 0 && aux.getJ() >= 0){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
+					}
+					else if(i == 1){// Arriba 
+						aux.setI(this.actual.getI()-1);
+						aux.setJ(this.actual.getJ());
+						if(aux.getI() >= 0){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
+					}
+					else if(i == 2){// Arriba a la derecha
+						aux.setI(this.actual.getI()-1);
+						aux.setJ(this.actual.getJ()+1);
+						if(aux.getI() >= 0 && aux.getJ()<this.dimensionM){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
+					}
+					else if(i == 3){// Izquierda
+						aux.setI(this.actual.getI());
+						aux.setJ(this.actual.getJ()-1);
+						if(aux.getJ() >= 0){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
+					}
+					else if(i == 4){// Derecha
+						aux.setI(this.actual.getI());
+						aux.setJ(this.actual.getJ()+1);
+						if(aux.getJ() < this.dimensionM){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
+					}
+					else if(i == 5){// Izquierda inferior
+						aux.setI(this.actual.getI()+1);
+						aux.setJ(this.actual.getJ()-1);
+						if(aux.getI() < this.dimensionN && aux.getJ() >= 0){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
+					}
+					else if(i == 6){// Abajo
+						aux.setI(this.actual.getI()+1);
+						aux.setJ(this.actual.getJ());
+						if(aux.getI() < this.dimensionN){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
+					}
+					else if(i == 7){// Derecha inferior
+						aux.setI(this.actual.getI()+1);
+						aux.setJ(this.actual.getJ()+1);
+						if(aux.getI() < this.dimensionN && aux.getJ() < this.dimensionM){// Esta bien
+							hacerLoMismo(this.matriz[aux.getI()][aux.getJ()], aux);
+						}
 					}
 				}
-				
 			}
+			// Elegir el siguiente nodo a tratar
+			this.actual = this.abierta.peek().getC();
+			coge = this.abierta.peek();
+			this.abierta.poll();
+			this.cerrada.add(coge);
 		}
 	}
-	
-	
+	public void hacerLoMismo(TNodo coge, Coordenadas aux){
+		// No tenemos que haber pasado por ahi
+		if(!coge.getUsado()){
+			coge.setUsado(true);
+			coge.setPadre(this.matriz[this.actual.getI()][this.actual.getJ()]);
+			coge.calcularH(aux, this.meta);
+			coge.calcularG();
+			coge.calcularF();
+			this.abierta.add(coge);
+		}
+	}
 }
--- Algoritmo-A-Estrella/src/CapaNegocio/TNodo.java
+++ Algoritmo-A-Estrella/src/CapaNegocio/TNodo.java
@@ -2,20 +2,54 @@ package CapaNegocio;
 
 public class TNodo {
 	private boolean usado;
-	private double h; 	// Formula h(n) = sqrt((x1+x2)^2+(y1+y2)^2)
+	private double h; 	// Formula h(n) = sqrt((x1-x2)^2+(y1-y2)^2)
 	private double g;	// Formula g(n) = SUMA(h(n))
 	private double f;	// Formula f(n) = h(n) + g(n)
 	private String tipo;
+	private TNodo padre;
+	private Coordenadas c;
 	
-	public TNodo(boolean usado, double h, double g, double f, String tipo){
+	public TNodo(boolean usado, double h, double g, double f, String tipo, Coordenadas c){
 		this.usado = usado;
 		this.h = h;
 		this.g = g;
 		this.f = f;
 		this.tipo = tipo;
+		this.c = c;
+		this.padre = null;
 	}
 
+	public void calcularG(){
+		this.g = this.h;
+		while(this.padre != null){
+			this.g += this.padre.h; 
+			this.padre = this.padre.getPadre();
+		}
+	}
+	public void calcularH(Coordenadas mias, Coordenadas meta){
+		double suma = Math.pow((mias.getI()-meta.getI()), 2)+Math.pow((mias.getJ()-meta.getJ()), 2);
+		this.h = Math.sqrt(suma);
+	}
+	public void calcularF(){
+		this.f = this.h+this.g;
+	}
+	
+	
 	// Empiezan los getters y los setters
+	public Coordenadas getC(){
+		return this.c;
+	}
+	public void setC(Coordenadas c){
+		this.c = c;
+	}
+	
+	public TNodo getPadre(){
+		return this.padre;
+	}
+	public void setPadre(TNodo padre){
+		this.padre = padre;
+	}
+	
 	public String getTipo(){
 		return this.tipo;
 	}
@@ -46,11 +80,11 @@ public class TNodo {
 		this.g = g;
 	}
 
-	public double getDistancia() {
+	public double getF() {
 		return this.f;
 	}
 
-	public void setDistancia(double f) {
+	public void setF(double f) {
 		this.f = f;
 	}
 	// Acaban los setter y getters
