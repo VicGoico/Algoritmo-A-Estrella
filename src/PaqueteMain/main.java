c98ca58a8545d0714244066037819e9433acabe0
VicGoico <33021153+WilsonIsAlive@users.noreply.github.com>
2019-03-11 12:27:10 +0100

    Nombre

--- src/PaqueteMain/main.java
+++ src/PaqueteMain/main.java
@@ -2,6 +2,9 @@ package PaqueteMain;
 import CapaNegocio.Logica;
 import CapaPresentacion.VentanaDimensiones;
 
+/*
+ * @Autor: Víctor Goicoechea Enrique 
+ * */
 public class main {
 	public static void main(String[] args) {		
 		VentanaDimensiones v = new VentanaDimensiones();
